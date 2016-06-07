db7ec77e65e2ec2c8eb96572444d4de05ac54b90
tkudryavtsev <tatiana.kudryavtseva@zalando.de>
2016-06-07 11:56:28 +0200

    Day 2

--- src/new_hope/week1.clj
+++ src/new_hope/week1.clj
@@ -1,9 +1,10 @@
 (ns new-hope.week1
   (:require [midje.sweet :refer :all]
-            [criterium.core :as crit]))
+            [clojure.test :refer :all]))
+
+
 
 ;; After launching the REPL always run (refresh) at least once
-
 ;; `comment` always returns nil and does not evaluate its arguments
 ;; Might be handy to type expressions here and send them to REPL
 ;;  instead of typing them in REPL directly
@@ -17,6 +18,8 @@
 
 (facts "some facts"
   (second [2 3 4]) => 3)
+
+(str 1 2)
 
 ;; For Week 1 days 1-3 just play around a bit, use comment form to write down expressions
 
@@ -44,6 +47,8 @@
   (conj '(2 3 4) 1)=>(list 1 2 3 4)
   (conj '(3 4) 2 1)=>(list 1 2 3 4))
 
+(comment
+ (ns-unmap *ns* 'str))
 
 ;; https://www.4clojure.com/problem/6
 ;; (= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
@@ -61,13 +66,12 @@
   (conj[1 2 3]4)=>[1 2 3 4]
   (conj[1 2 ]3 4)=>[1 2 3 4])
 
-;; TODO: exception if compare it with {:c :b :d :a}
 ;; https://www.4clojure.com/problem/8
 ;; (= __ (set '(:a :a :b :c :c :c :c :d :d)))
 ;; (= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))
 (facts "about Sets"
-  (set '(:a :a :b :c :c :c :c :d :d))=> #{:a :b :c :d}
-  (clojure.set/union #{:a :b :c} #{:b :c :d}) => #{:a :b :c :d})
+  (set '(:a :a :b :c :c :c :c :d :d))=> #{:c :b :d :a}
+  (clojure.set/union #{:a :b :c} #{:b :c :d}) => #{:c :b :d :a})
 
 (comment
  (clojure.set/union #{:a :b :c} #{:b :c :d}))
@@ -78,7 +82,6 @@
   (conj #{1 4 3} 2)=> #{1 2 3 4})
 
 ;; https://www.4clojure.com/problem/10
-
 ;; (= __ ((hash-map :a 10, :b 20, :c 30) :b))
 ;; (= __ (:b {:a 10, :b 20, :c 30}))
 (facts "about hash-map"
@@ -101,9 +104,95 @@
   (second [2 3 4]) => 3
   (last (list 1 2 3)) => 3)
 
+;; Day 2
+;; https://www.4clojure.com/problem/13
+;; (= __ (rest [10 20 30 40]))
+(facts "about Seq. rest"
+  (rest [ 10 20 30 40]) =>[20 30 40])
 
+;; https://www.4clojure.com/problem/14
+;; (= __ ((fn add-five [x] (+ x 5)) 3))
+;; (= __ ((fn [x] (+ x 5)) 3))
+;; (= __ (#(+ % 5) 3))
+;; (= __ ((partial + 5) 3))
 
+(deftest problem-14
+  (is (= 8 ((fn add-fixe [x] (+ x 5)) 3)))
+  (is (= 8 ((fn [x] (+ x 5)) 3)))
+  (is (= 8 (#(+ % 5) 3)))
+  (is (= 8 ((partial + 5) 3))))
+
+(test #'new-hope.week1/problem-14)
+
+;; https://www.4clojure.com/problem/15
+;; (= (__ 2) 4)
+;; (= (__ 3) 6)
+;; (= (__ 11) 22)
+;; (= (__ 7) 14)
 ;;
+(deftest problem-15
+  (is (= 4 ((partial * 2) 2)))
+  (is (= 6 ((partial * 2) 3)))
+  (is (= 22 ((partial * 2) 11)))
+  (is (= 14 ((partial * 2) 7))))
+
+;; same problem but different test:
+
+(test #'new-hope.week1/problem-15)
+
+(defn double "this is double func"
+  {:test #(do
+            (assert (= 14 (double 7))))}
+  [number] ((partial * 2) number))
+
+(test #'new-hope.week1/double)
+
+;; https://www.4clojure.com/problem/16
+;; (= (__ "Dave") "Hello, Dave!")
+;; (= (__ "Jenn") "Hello, Jenn!")
+;; (= (__ "Rhea") "Hello, Rhea!")
+
+(defn concat-str "this is string merge func"
+  {:test #(do
+            (assert (=(concat-str "Dave")"Hello, Dave!")))}
+  [person] (str "Hello, " person "!"))
+
+(test #'new-hope.week1/concat-str)
+
+;; https://www.4clojure.com/problem/17
+;; (= __ (map #(+ % 5) '(1 2 3)))
+(deftest test-the-map
+  (is(= (6 7 8) (map #(+ %5) '(1 2 3)))))
+
+(test #'new-hope.week1/test-the-map)
+
+;; https://www.4clojure.com/problem/18
+;; (= __ (filter #(> % 5) '(3 4 5 6 7)))
+;; 6 7 grater than 5?
+(deftest problem-18
+  (is (= (6 7) (filter #(> %5) '(3 4 5 6 7)))))
+
+(test #'new-hope.week1/problem-18)
+
+;; https://www.4clojure.com/problem/35
+;; (= __ (let [x 5] (+ 2 x)))
+;; (= __ (let [x 3, y 10] (- y x)))
+;; (= __ (let [x 21] (let [y 3] (/ x y))))
+
+(deftest problem-35
+  (is (= 7 (let [x 5] (+ 2 x))))
+  (is (= 7 (let [x 3, y 10] ( - y x))))
+  (is (= 7 (let [x 21] (let [y 3](/ x y))))))
+
+(test #'new-hope.week1/problem-35)
+
+
+;; Day 3. Recursion
+;; https://www.4clojure.com/problem/37
+;;
+
+
+
 ;; Day 4
 ;;
 
@@ -113,7 +202,7 @@
 
 (comment
   (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d)))
-  (= __ (let [x 5] (+ 2 x))))
+  (= 7 (let [x 5] (+ 2 x))))
 
 
 (comment
